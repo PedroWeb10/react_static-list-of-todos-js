2a7059802847ea09cff45d7e3cd739c220c9ffe8
PedroWeb10 <pdro_dev@yahoo.com>
2025-04-02 10:35:34 -0300

    React Static List of TODOs 1.4

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,15 +1,12 @@
 // Add the required props
 export const TodoInfo = ({ todo }) => (
-
-    <article
-      key={todo.id}
-      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
-    >
-      <h2 className="TodoInfo__title">{todo.title}</h2>
-      <a className="UserInfo" href={`mailto:${todo.user.email}`}>
-        {todo.user.name}
-      </a>
-    </article>
-
-
+  <article
+    key={todo.id}
+    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    <a className="UserInfo" href={`mailto:${todo.user.email}`}>
+      {todo.user.name}
+    </a>
+  </article>
 );
